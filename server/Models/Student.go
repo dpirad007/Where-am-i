f1ce520511eeba89777979541725eec18c02264d
vedpatil611 <vedpatil611@gmail.com>
2022-03-23 14:57:34 +0530

    Document reading sample

--- server/Models/Student.go
+++ server/Models/Student.go
@@ -1,10 +1,14 @@
 package Models
 
 type Student struct {
-    ID string
-    Name string
-    RollNo string
-    PhoneNo []string
-    Email []string
-    ParentPhoneNo []string
+    ID string                   `bson:"_id"`
+    Name string                 `bson:"Name"`
+    RollNo string               `bson:"RollNo"`
+    PhoneNo []string            `bson:"PhoneNo"`
+    Email []string              `bson:"Email"`
+    ParentPhoneNo []string      `bson:"ParentPhoneNo"`
+}
+
+func GetStudent(rollNo string) {
+
 }
--- server/main.go
+++ server/main.go
@@ -1,10 +1,47 @@
 package main
 
 import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+	// "where-am-i-server/Models"
 	"where-am-i-server/Routes"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+func ConnectDB() (*mongo.Client, context.Context) {
+    ctx, _ := context.WithTimeout(context.Background(), 10 * time.Second)
+    client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    return client, ctx
+}
+
 func main() {
+    DB, ctx := ConnectDB()
+
+    var result []bson.D
+    filter := bson.D {}
+    option := bson.D {}
+
+    cursor, _ := DB.Database("WhereAmI").Collection("Students").Find(ctx, filter, options.Find().SetProjection(option))
+
+    if err := cursor.All(ctx, &result); err != nil {
+        log.Fatal(err)
+    }
+
+    for _, doc := range result {
+        fmt.Println(doc)
+    }
+    // fmt.Printf("%+v\n", result)
+
     router := Routes.SetupRouter()
-	router.Run("localhost:8080")
+    router.Run("localhost:8080")
 }
